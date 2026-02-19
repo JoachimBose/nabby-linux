8762e6c530866ae44112c28c9051de9d7acc6f1a
JoachimBose <joachimbose48@gmail.com>
2026-02-19 17:34:14 +0100

    Added: fork for f1c200s ethernet

---
+++ board/lctech-pi-ether/overlay/etc/init.d/S51nabby_init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cat > /dev/console << EOF
+
+             _     _                #
+ _ __   __ _| |__ | |__  _   _      #---###
+| '_ \ / _` | '_ \| '_ \| | | |    / O O \
+| | | | (_| | |_) | |_) | |_| |   |   '   |
+|_| |_|\__,_|_.__/|_.__/ \__, |   |       |
+    linux hobby project  |___/    '--___--'
+EOF
+echo "we have code execution!" > /dev/console
