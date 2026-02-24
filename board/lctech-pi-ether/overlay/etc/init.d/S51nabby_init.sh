877163e97352e4f68e1198b54c5100ddbd65f1d9
JoachimBose <joachimbose48@gmail.com>
2026-02-24 18:45:55 +0100

    fix: banner

--- board/lctech-pi-ether/overlay/etc/init.d/S51nabby_init.sh
+++ board/lctech-pi-ether/overlay/etc/init.d/S51nabby_init.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-echo "             _     _                #" > /dev/console
-echo " _ __   __ _| |__ | |__  _   _      #---###" > /dev/console
-echo "| '_ \ / _` | '_ \| '_ \| | | |    / O O \" > /dev/console
-echo "| | | | (_| | |_) | |_) | |_| |   |   '   |" > /dev/console
-echo "|_| |_|\__,_|_.__/|_.__/ \__, |   |       |" > /dev/console
-echo "    linux hobby project  |___/    '--___--'" > /dev/console
+cat /root/banner.txt > /dev/console
 
 echo "we have code execution!" > /dev/console
