9945c5742f72ddff34e430e0550afb9d3b757bf5
JoachimBose <joachimbose48@gmail.com>
2026-03-22 15:28:21 +0000

    fix: wrong prefix

--- package/spookystats/spookystats.mk
+++ package/spookystats/spookystats.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-SPOOKY_STATS_VERSION = 1.0
-SPOOKY_STATS_SITE = $(BR2_EXTERNAL_NABBY_LINUX_PATH)/package/spookystats
-SPOOKY_STATS_SITE_METHOD = local
-SPOOKY_STATS_INSTALL_TARGET = YES
+SPOOKYSTATS_VERSION = 1.0
+
+SPOOKYSTATS_SITE = /home/vagrant/nabby-linux/package/spookystats
+SPOOKYSTATS_SITE_METHOD = local
+SPOOKYSTATS_INSTALL_TARGET = YES
 
 $(eval $(cmake-package))
