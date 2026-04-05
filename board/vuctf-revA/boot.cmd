9d62f6ad3487d0c49ac1b5c6f8a1f27004eb4886
Joachim Bose <joachimbose48@gmail.com>
2026-04-05 12:03:29 +0000

    Fix: uboot now restricts cli

--- board/vuctf-revA/boot.cmd
+++
@@ -1,5 +0,0 @@
-setenv bootargs console=ttyS0,115200n8 rootwait root=/dev/mtdblock3 rw rootfstype=jffs2
-sf probe 0
-sf read 0x80c00000 0x80000 0x10000
-sf read 0x80008000 0x90000 0x380000
-bootz 0x80008000 - 0x80C00000
